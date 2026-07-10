c5181a82a76d3afb8f0bf054e1b69a34d729630f
Bhagyash-raut <bhagyashraut9@gmail.com>
2026-07-10 10:25:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,28 +1,3 @@
-variable "project" {
-  type    = string
-  default = "lion"
-}
-
-variable "desired_nodes" {
-  type    = number
-  default = 2
-}
-
-variable "min_nodes" {
-  type    = number
-  default = 1
-}
-
-variable "max_nodes" {
-  type    = number
-  default = 3
-}
-
-variable "node_instance_type" {
-  type    = string
-  default = "c7i-flex.large"
-}
-
 project            = "lion"
 desired_nodes      = 2
 min_nodes          = 1
